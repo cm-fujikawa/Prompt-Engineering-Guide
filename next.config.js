25decf27dd2fd83a04628b5d168881fddf0f6a45
cm-fujikawa <68237804+cm-fujikawa@users.noreply.github.com>
2025-12-14 23:48:41 +0900

    fix: Disable automatic locale detection in Next.js configuration

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ module.exports = withNextra({
   i18n: {
     locales: ['en', 'zh', 'jp', 'pt', 'tr', 'es', 'it', 'fr', 'kr', 'ca', 'fi', 'ru','de', 'ar'],
     defaultLocale: 'en',
+    localeDetection: false,
   },
   webpack(config) {
     const allowedSvgRegex = /components\/icons\/.+\.svg$/
